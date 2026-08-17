b9085be06422e7459ea25b0d5b2d4f5da39954fc
Harsh Soni <45728272+harshh06@users.noreply.github.com>
2026-08-17 12:16:15 -0500

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mp = defaultdict(list)
+        for a, b in prerequisites:
+            mp[b].append(a)
+        vis = [0] * numCourses
+        
+        def isCycle(course: int) -> bool:
+            vis[course] = 1
+            for adjCourse in mp[course]:
+                if not vis[adjCourse]:
+                    if isCycle(adjCourse):
+                        return True
+                elif vis[adjCourse] == 1:
+                    return True
+                elif vis[adjCourse] == 2:
+                    continue #do nothing!! 
+            vis[course] = 2
+            return False
+        
+        for course in range(numCourses):
+            if not vis[course]:
+                if not vis[course] and isCycle(course):
+                    return False
+        for course in vis:
+            if not course:
+                return False
+        return True
+
+            
